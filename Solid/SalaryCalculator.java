7e4a959c06d71b710687881e500df73380910234
Ayala217 <140421850+Ayala217@users.noreply.github.com>
2024-08-02 14:27:59 -0500

    Update SalaryCalculator.java

--- Solid/SalaryCalculator.java
+++ Solid/SalaryCalculator.java
@@ -1,10 +1,10 @@
 import Employees.Employee;
 
 public class SalaryCalculator {
-
+int ModificadorSalarioPartTime = 2;
     public static int calculateSalary(Employee employee) {
         if (employee.tiempoParcial()) {
-            return (employee.Versalario() / 2);
+            return (employee.Versalario() / ModificadorSalarioPartTime);
         } else {
             return employee.Versalario();
         }
